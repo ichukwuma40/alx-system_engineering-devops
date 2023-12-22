80fdb1293c504bd2e6bcbb09a54127affa6f1ae8
Innocent Chukwuma <ichukwuma40@gmail.com>
2023-12-22 12:16:38 -0800

    config mgt

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
 #!/usr/bin/pup
 #install an  especific version of fask(2.1.0)
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+ package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+
 }
